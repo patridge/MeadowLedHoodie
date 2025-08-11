a20582025257a720316cfb2eb0296fb4341081b2
Adam Patridge <patridge@gmail.com>
2025-08-11 12:24:19 -0700

    Extract ColorHelpers out to shared Meadow library

--- LedHoodie-ProjectLab/MeadowApp.cs
+++ LedHoodie-ProjectLab/MeadowApp.cs
@@ -7,6 +7,7 @@ using System;
 using System.Collections.Generic;
 using System.Numerics;
 using System.Threading.Tasks;
+using LedHoodiesCommon;
 
 namespace ProjectLabLedHoodie;
 
---
+++ LedHoodieCommon/ColorHelpers.cs
@@ -0,0 +1,98 @@
+using System;
+using Meadow;
+
+namespace LedHoodiesCommon;
+
+/// <summary>
+/// Helper methods for working with Meadow colors.
+/// </summary>
+public static class ColorHelpers
+{
+    /// <summary>
+    /// Convert HSV/HSB values to a System.Drawing.Color (based on RGB, with some HSL mismatch thrown in).
+    /// </summary>
+    /// <param name="h">Hue: [0..360)</param>
+    /// <param name="s">Saturation: [0..1]</param>
+    /// <param name="v">Value/Brightness: [0..1]</param>
+    public static Color HsvToRgb(int h, float s, float v)
+    {
+        var hue = h;
+        var saturation = s;
+        var brightness = v;
+
+        var r = brightness;
+        var g = brightness;
+        var b = brightness;
+
+        // If saturation is 0, all colors are the same (some flavor of gray).
+        if (saturation != 0)
+        {
+            // Calculate appropriate sector of a 6-part color wheel.
+            var sectorPosition = hue / 60f;
+            var sectorNumber = (int)Math.Floor(sectorPosition);
+
+            // Fractional part of the sector (how many degrees into sector).
+            var fractionalSector = sectorPosition - sectorNumber;
+
+            // Calculate values on the three axes of the color.
+            var p = brightness * (1 - saturation);
+            var q = brightness * (1 - (saturation * fractionalSector));
+            var t = brightness * (1 - (saturation * (1 - fractionalSector)));
+
+            // Assign fractional colors to red, green, and blue based on angle's sector.
+            switch (sectorNumber)
+            {
+                case 0:
+                case 6:
+                    r = brightness;
+                    g = t;
+                    b = p;
+                    break;
+                case 1:
+                    r = q;
+                    g = brightness;
+                    b = p;
+                    break;
+                case 2:
+                    r = p;
+                    g = brightness;
+                    b = t;
+                    break;
+                case 3:
+                    r = p;
+                    g = q;
+                    b = brightness;
+                    break;
+                case 4:
+                    r = t;
+                    g = p;
+                    b = brightness;
+                    break;
+                case 5:
+                    r = brightness;
+                    g = p;
+                    b = q;
+                    break;
+            }
+        }
+
+        r *= 255;
+        g *= 255;
+        b *= 255;
+
+        return Color.FromRgb((int)r, (int)g, (int)b);
+    }
+
+    /// <summary>
+    /// Using the provided Random (or using a new one, if null), generate a random HSV color (0..360 hue on full saturation and brightness).
+    /// </summary>
+    /// <param name="rand">The Random instance to use for generating the color. If null, a new Random will be created.</param>
+    /// <returns>A random, bright Meadow Color.</returns>
+    public static Color GetRandomColor(Random rand)
+    {
+        rand ??= new Random();
+
+        int hue = rand.Next(360 - 1);
+        return HsvToRgb(hue, 1, 1);
+    }
+}
